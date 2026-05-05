3b551186c5a5d846a34596db3f5dd841bcf58d64
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-05 12:28:49 +0530

    evenorodd

---
+++ Specialisation/oddOrEven.js
@@ -0,0 +1,7 @@
+num1=2;
+num2=5;
+test1=num1%2
+test2=num2%2
+
+console.log(num1,"is an even number?: ",test1==0)
+console.log(num2,"is an even number?: ",test2==0)
--- Specialisation/operators.js
+++ Specialisation/operators.js
@@ -1,3 +1,5 @@
 var timeRemaining =0;
 var energy =0;
 console.log("Game Over:",timeRemaining ==0|| energy ==0);
+
+
