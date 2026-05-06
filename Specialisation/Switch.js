b70fa3d624990e3394fc4d2ce2b428d5e69c2afd
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-06 12:11:59 +0530

    Create Switch.js

---
+++ Specialisation/Switch.js
@@ -0,0 +1,15 @@
+var Place="first"
+
+switch (Place){
+    case 'second':
+        console.log("Silver");
+        break;
+
+    case 'first':
+        console.log("Gold");
+        break;
+        
+    default:
+        console.log("no price ");
+        break;
+}   
