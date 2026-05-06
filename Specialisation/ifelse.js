a6651c6ea9cca1d0061141a067e7f3b95c48c91b
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-06 12:08:30 +0530

    Update ifelse.js

--- Specialisation/ifelse.js
+++ Specialisation/ifelse.js
@@ -1,7 +1,11 @@
 var result= 500
-if (result >100){
+if (result==500){
+    console.log("Perfect score")
+}
+else if (result >100){
     console.log("You passed")
 }
+
 else {
     console.log("Failed. Try again")
 }
