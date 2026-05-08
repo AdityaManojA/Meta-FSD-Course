73c20b386e1fa9a094bf3dff78caae9278fee2b1
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-08 12:41:51 +0530

    Create loopAndFunction.js

---
+++ Specialisation/loopAndFunction.js
@@ -0,0 +1,7 @@
+function  printLoop(array1){ //array1 is the parameter
+    for(let i=0;i<array1.length;i++){
+        console.log(i+1,array1[i]);
+    }
+}
+Colors=['red','blue','green','orange','apple'];
+printLoop(Colors);
