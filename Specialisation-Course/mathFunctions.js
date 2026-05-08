d1042c9dde0d3efba0ee183defb87863199902d9
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-08 13:54:22 +0530

    random

--- Specialisation-Course/mathFunctions.js
+++ Specialisation-Course/mathFunctions.js
@@ -2,4 +2,3 @@ console.log(Math.round(2.65463))
 console.log(Math.trunc(2.65463))
 console.log(Math.sqrt(16))
 console.log(Math.cbrt(16))
-    
---
+++ Specialisation-Course/mathRandom.js
@@ -0,0 +1,4 @@
+var dec =Math.random()
+console.log(dec);
+dec1=Math.trunc(dec*100)
+console.log(dec1)
