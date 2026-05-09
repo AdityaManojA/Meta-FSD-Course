897d792991e82dc9382613b44bbc533987f6b313
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-09 22:47:26 +0530

    1

--- Specialisation-Course/stringFunctions.js
+++ Specialisation-Course/stringFunctions.js
@@ -4,3 +4,5 @@ console.log(greet.length)
 console.log(greet.charAt(4));
 console.log(greet.indexOf("w"))
 console.log(greet.split("e"))
+console.log(greet.toLowerCase())
+console.log(greet.toUpperCase())
