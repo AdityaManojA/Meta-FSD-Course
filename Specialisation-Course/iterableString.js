31030fdacefbdba3a993b3a692d52f05237e64fe
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-09 22:34:10 +0530

    1

---
+++ Specialisation-Course/iterableString.js
@@ -0,0 +1,5 @@
+var alphabhet='abcdefghi';
+
+for(let i=0;i<alphabhet.length;i++){
+    console.log(alphabhet[i])
+}
---
+++ Specialisation-Course/stringFunctions.js
@@ -0,0 +1,6 @@
+var greet="Hello world"
+
+console.log(greet.length)
+console.log(greet.charAt(4));
+console.log(greet.indexOf("w"))
+console.log(greet.split("e"))
