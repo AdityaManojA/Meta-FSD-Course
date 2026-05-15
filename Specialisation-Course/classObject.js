d9cce2a33bc07f1de0060220e321c0486d08c6d7
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-15 20:15:42 +0530

    1

---
+++ Specialisation-Course/classObject.js
@@ -0,0 +1,8 @@
+class Train{
+    constructor(color,lightsOn){
+        this.color=color;
+        this.lightsOn=lightsOn;
+    }
+}
+var scuba=new Train('red',false)
+console.log(scuba)
---
+++ Specialisation-Course/classObjectFuture.js
@@ -0,0 +1,26 @@
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
+}
+
+var Train1=new Train("green",false)
+
+Train1.toggleLights()
+
+Train1.getPrototype()
+Train1.getSelf()
