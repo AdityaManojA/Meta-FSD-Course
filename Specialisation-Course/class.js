d4fd9549a43854530884466dbcd7bafa03d5ca9a
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-15 19:32:13 +0530

    1

---
+++ Specialisation-Course/class.js
@@ -0,0 +1,9 @@
+class Car{
+    constructor(color,speeed){
+        this.speed=this.speed;
+        this.color=color;
+    }
+    turboOn(){
+        console.log("Turbo is on , High speed caution!")
+    }
+}
