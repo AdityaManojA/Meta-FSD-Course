f34891e0557fb8b0188e8e0a5c19f2639f769ef6
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-17 14:57:26 +0530

    Update forEach.js

--- Specialisation-Course/forEach.js
+++ Specialisation-Course/forEach.js
@@ -1,5 +1,16 @@
-const fruits = ['kiwi','mango','apple','pear'];
-function appendIndex(fruit, index) {
-    console.log(`${index}. ${fruit}`)
-}
-fruits.forEach(appendIndex);
+const fruits = ['apple', 'banana', 'cherry'];
+
+fruits.forEach((fruit,index,arr) => {
+    console.log(`index ${index}: ${fruit},[${arr}]`)
+});
+
+
+fruits.forEach(((a,b,c)=>console.log(`index ${a}: ${b},[${c}]`)))
+const nums=[1,2,3,4,5];
+
+
+nums.forEach(num=>console.log(num*2))
+
+nums.forEach((number2)=>{
+    console.log(`${number2*2}`);
+});
