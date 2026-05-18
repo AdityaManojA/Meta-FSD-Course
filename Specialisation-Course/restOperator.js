77abf2378868e46775300f82abb35c3ff207517f
Aditya Manoj <130238815+AdityaManojA@users.noreply.github.com>
2026-05-18 11:44:22 +0530

    Update restOperator.js

--- Specialisation-Course/restOperator.js
+++ Specialisation-Course/restOperator.js
@@ -4,3 +4,22 @@ function trackScores(teamName, ...scores) {
 }
 
 trackScores('madrid','2-0','1-1')
+
+
+  let obj = {
+        key: 1,
+        value: 4
+    };
+
+    let output = { ...obj };
+    output.value -= obj.key;
+
+    console.log(output.value);
+
+
+    
+    function count(...basket) {
+        console.log(basket.length)
+    }
+
+    count(10, 9, 8, 7, 6);
